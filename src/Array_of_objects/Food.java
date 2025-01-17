c059ee2d534d0b824dadeabb5b0219fb76d2a02c
R.O.H.I.T.H <168300495+code-by-rohith@users.noreply.github.com>
2025-01-17 15:49:34 +0530

    first commit

--- src/Array_of_objects/Food.java
+++ src/Array_of_objects/Food.java
@@ -2,15 +2,12 @@ package Array_of_objects;
 
 public class Food {
     public static void main(String[] args) {
-       Pizza[] pizza = new Pizza[3];
+       Pizza[] pizza ;
        Pizza data = new Pizza("humoi");
         Pizza data1 = new Pizza("3r");
         Pizza data2 = new Pizza("tntntg");
-
-        pizza[0] = data;
-        pizza[1] = data1;
-        pizza[2] = data2;
-        System.out.println(pizza[0].name1);
+        pizza = new Pizza[]{data, data1, data2};
+        System.out.println(pizza[1].name1);
 
 
     }
